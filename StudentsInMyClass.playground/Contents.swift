092e2b2cdfd62612841825144539c76e8b122830
Chidiemeh184 <chidiemeh184@gmail.com>
2016-03-09 16:57:11 -0500

    Added files via upload
    updated

--- StudentsInMyClass.playground/Contents.swift
+++ StudentsInMyClass.playground/Contents.swift
@@ -1,3 +1,24 @@
+//=============================================================
+//  NAME: CHIDI EMEH
+//  COURSE: CSCI 2994 (iOS Programming Fundamentals)
+//  FILENAME: StudentsInMyClass.playground
+//  DATE: 2/16/2016
+//  EXERCISE 2: This program shows basic string manipulation
+//              and the use of ranges
+//=============================================================
+
+/*:
+**Exercise:** Get Acquainted. Get a few properties of each student in your class... name and email are good. Use the `String` and `Character` as well as the data types and operators in [Basics | Data Types | Operators](Basics%20|%20Data%20Types%20|%20Operators) to complete this exercise.
+>> **Example Output:**
+* `My name is Mathew Sheets, my email is swift.dude@gmail.com.`
+* `Other students in my class are Annie, Sam, Jack, Hudson and Oliver.`
+* `Oliver dropped this class.` (**use the second bullet point string to pull out the last students's name.**)
+* `Other students in my class are Annie, Sam, Jack, Hudson and CARSON was added.`  (**use the second bullet point string and append another student.**)
+* `Jack and Hudson are in the class.` (**use the second bullet point string to pull out third and fourth student.**)
+>> **Constraints:**
+* Use a nested tuple to hold your students
+* Use the string created from the second bullet point for all other string creation
+*/
 
 import UIKit
 import Foundation
@@ -12,13 +33,6 @@ let iosClassMates = (
 )
 
 let iosClassMatesNewStudentAdded = (firstName: "Carson", lastName: "Payne", email: "cpayne@cscc.edu" )
-
-let mirrow = Mirror(reflecting: iosClassMates)
-
-for (label) in mirrow.children {
-    //print(label.value)
-}
-
 
 let me = "My name is \(iosClassMates.0.firstName) \(iosClassMates.0.lastName), my email is \(iosClassMates.0.email)."
 
